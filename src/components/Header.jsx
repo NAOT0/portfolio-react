8f643cae61fb260bc295389eed777fa06a87235b
NAOT0 <NAOT0@example.com>
2025-02-11 23:46:13 +0900

    homeをportfolio-reactに変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/portfolio-react">Home</Link>
           </li>
           <li>
             <Link to="/blog">Blog</Link>
